3080c02a0b5cdbc6c047c453f9b70eca0931200e
Henrik Bjørhall <47069120+Orphanum@users.noreply.github.com>
2019-08-22 14:49:10 +0200

    Update Village.cs

--- TextGame/Story/Village.cs
+++ TextGame/Story/Village.cs
@@ -20,6 +20,10 @@ namespace TextGame.Story
         public override void SearchLocation()
         {
             Program.HasShovel = true;
+            Console.WriteLine("You take a closer look around, and there is indeed no people here.");
+            Console.WriteLine("You find rotten remains of food, and personal belongings, but no signs of life.");
+            Console.WriteLine("As you're about to exit the village, you notice a shovel.");
+            Console.WriteLine("Might as well bring it.");
 
         }
     }
