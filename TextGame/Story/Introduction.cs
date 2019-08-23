4ef905e97ab10bea8886a60c64a8299e9a058a92
Henrik Bjørhall <47069120+Orphanum@users.noreply.github.com>
2019-08-23 13:47:42 +0200

    Update Introduction.cs

--- TextGame/Story/Introduction.cs
+++ TextGame/Story/Introduction.cs
@@ -15,6 +15,11 @@ namespace TextGame.Story
             Console.WriteLine("You wake slowly. Your back aches and your body is sore. The sky is black, you see neither the moon nor any stars.");
             Console.WriteLine("The last thing you remember is going to sleep in your bed. Where you have woken up, however, is a different place completely.");
             Console.WriteLine("You do not recognize where you are, but judging from the lack of buildings and the surrounding terrain, you must be somewhere in the countryside.");
+            Console.WriteLine("\n");
+            Console.WriteLine("There's several paths forking out around you.");
+            Console.WriteLine("A quick look around you reveals a forest.");
+            Console.WriteLine("A bit further in the distance you can see a village, with a nearby beach.");
+            Console.WriteLine("The last path leads to a graveyard behind the village.\n");
         }
     }
 }
