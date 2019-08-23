de0bdbae95dddc547695cf26fb9dcbc2621e06a5
Henrik Bjørhall <47069120+Orphanum@users.noreply.github.com>
2019-08-23 13:46:03 +0200

    more story

--- TextGame/Story/Crypt.cs
+++ TextGame/Story/Crypt.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextGame.Artwork;
 
 namespace TextGame.Story
 {
@@ -15,10 +16,16 @@ namespace TextGame.Story
         }
         public override void MainStory()
         {
-            Console.WriteLine("placeholder crypt");
+            Console.WriteLine("The crypt is in the same decrepit state as the rest of the graveyard.");
+            Console.WriteLine("The construction appears to be of a higher quality however.");
+            Console.WriteLine("Inside there's a stone stair that leads further down.");
         }
         public override void SearchLocation()
         {
+            Console.WriteLine("\nYou decide to head further in. At the end of the stairs there's a room.");
+            Console.WriteLine("At the centre is a stone sarcophagus, the lid has been pushed off.");
+            Console.WriteLine("Inside, there's a golden key, but nothing else, and certainly no corpse.");
+            Key.ArtKey();
             Program.HasKey = true;
         }
     }
--- TextGame/Story/Graveyard.cs
+++ TextGame/Story/Graveyard.cs
@@ -15,11 +15,16 @@ namespace TextGame.Story
         }
         public override void MainStory()
         {
-            Console.WriteLine("placeholder graveyard");
+            Console.WriteLine("You arrive at the graveyard. The atmosphere is near terrifying.");
+            Console.WriteLine("The blackened sky combined with the spooky sounds makes this a place you do NOT want to stay in.");
         }
         public override void SearchLocation()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("\nDespite your gut feeling you decide to take a closer look around the area.");
+            Console.WriteLine("Most of the graves here are old and decrepit. Clearly nobody is caring for them.");
+            Console.WriteLine("The writing is hardly readable. At the centre of the graveyard, there's a crypt.");
+            Console.WriteLine("It appears to be unlocked.");
+
         }
     }
 }
